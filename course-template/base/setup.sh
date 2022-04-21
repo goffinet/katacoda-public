11e88348a7cb4c319f5b0fa98a31dee7da68d198
goffinet <goffinet@goffinet.org>
2022-04-21 09:14:11 +0200

    got to killercoda

--- course-template/base/setup.sh
+++ course-template/base/setup.sh
@@ -9,15 +9,15 @@ ssh-keygen -b 4096 -t rsa -f /root/.ssh/id_rsa_ansible -q -N ""
 cat /root/.ssh/id_rsa_ansible.pub > $share/authorized_keys
 docker run --name node0 -v $share:$share --network $network --rm --privileged -d ${image}
 node0=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' node0)
-docker exec node0 /bin/bash -c 'cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys'
+docker exec node0 /bin/bash -c "cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys"
 docker run --name node1 -v $share:$share --network $network --rm --privileged -d ${image}
 sudo su -c "echo $node0 node0 >> /etc/hosts"
 node1=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' node1)
-docker exec node1 /bin/bash -c 'cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys'
+docker exec node1 /bin/bash -c "cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys"
 docker run --name node2 -v $share:$share --network $network --rm --privileged -d ${image}
 sudo su -c "echo $node1 node1 >> /etc/hosts"
 node2=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' node2)
-docker exec node2 /bin/bash -c 'cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys'
+docker exec node2 /bin/bash -c "cp -r $share/authorized_keys /home/user/.ssh/ ; chown user /home/user/.ssh/authorized_keys"
 sudo su -c "echo $node2 node2 >> /etc/hosts"
 cat << EOF > /etc/ansible/hosts
 node0 ansible_host=$node0
@@ -38,4 +38,4 @@ callback_whitelist = profile_tasks
 [callback_profile_tasks ]
 task_output_limit = 100
 EOF
-#clear
+clear
